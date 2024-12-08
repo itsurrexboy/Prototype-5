6acce9a8960c1add5112809e2f8644feafa128df
- manas <rexboy3825@gmail.com>
2024-12-08 23:42:13 +0530

    Added Challenge 5 Files

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score : " + score;
+        scoreText.text = "" + score;
     }
 
     public void GameOver(){
